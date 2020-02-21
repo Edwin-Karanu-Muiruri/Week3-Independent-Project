46c25b12c9def3a8cc4b30bd042c6f22b3e106d7
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-02-21 17:31:27 +0300

    Added jQuery functions to toggle between the icons in the portfilio and the description paragraphs.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("designIcon").click(function(){
+        $("imgDesign").toggle();
+        $("paragraphDesign").toggle();
+    })
+});
+$(document).ready(function(){
+    $("devIcon").click(function(){
+        $("imgDev").toggle();
+        $("paragraphDev").toggle();
+    })
+});
+$(document).ready(function(){
+    $("productIcon").click(function(){
+        $("imgProduct").toggle();
+        $("paragraphProduct").toggle();
+    })
+});
